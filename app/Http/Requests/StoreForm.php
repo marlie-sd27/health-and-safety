4db524b51ee35231ca08821d93f8f96240296e0c
marlie.dueck <marlie.dueck@sd27.bc.ca>
2020-07-10 10:45:37 -0700

    Sanitized section and field inputs

--- app/Http/Requests/StoreForm.php
+++ app/Http/Requests/StoreForm.php
@@ -46,6 +46,30 @@ class StoreForm extends FormRequest
     // sanitize the user input
     protected function prepareForValidation()
     {
+        // sanitize each section title, section description, label and options
+        $section_titles = array();
+        $section_descriptions = array();
+        $labels = array();
+        $options = array();
+
+        if (isset($this->section_title))
+        {
+            foreach ($this->section_title as $key => $value) {
+                $section_titles[$key] = filter_var($value, FILTER_SANITIZE_STRING);
+                $section_descriptions[$key] = filter_var($this->section_description[$key], FILTER_SANITIZE_STRING);
+            }
+        }
+
+        if (isset($this->label))
+        {
+            foreach ($this->label as $key => $value)
+            {
+                $labels[$key] = filter_var($value, FILTER_SANITIZE_STRING);
+                $options[$key] = filter_var($this->options[$key], FILTER_SANITIZE_STRING);
+            }
+        }
+
+
         $this->merge([
             'title' => filter_var($this->form_title, FILTER_SANITIZE_STRING),
             'description' => filter_var($this->form_description, FILTER_SANITIZE_STRING),
@@ -56,8 +80,11 @@ class StoreForm extends FormRequest
             'required_role' => filter_var($this->required_role, FILTER_SANITIZE_STRING),
             'full_year' => isset($this->full_year),
 
+            'section_title' => $section_titles,
+            'section_description' => $section_descriptions,
 
+            'label' => $labels,
+            'options' => $options,
         ]);
-
     }
 }
