8fc5a694ff87fd100e045f60c7cfed174df53210
marlie.dueck <marlie.dueck@sd27.bc.ca>
2020-08-24 11:50:27 -0700

    Added link for events route

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,5 +1,10 @@
 @extends('layout')
 
 @section('content')
+    @if(Auth::user()->isAdmin())
+        <div class="container">
+            <a href="{{ route('events') }}">List all event deadlines..</a>
+        </div>
+    @endif
     <div id="calendar"></div>
 @endsection
