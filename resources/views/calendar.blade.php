ed8ecea9c95e4ba0951fe54321e1403a6a01f103
marlie.dueck <marlie.dueck@sd27.bc.ca>
2020-08-24 12:30:16 -0700

    Fixed calendar events for non-principals and non-admins

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,15 +6,5 @@
             <a href="{{ route('events') }}">List all event deadlines..</a>
         </div>
     @endif
-    @if(!Auth::user()->isPrincipal())
-        <div class="alert alert-danger alert-dismissible alert-dismissible fade show" role="alert">
-            <p>Currently, teachers are not able to view calendar events. This is a known issue and we are
-                working on resolving it as soon as possible. Thank you for your patience. In the meantime, you can still
-                view upcoming deadlines from your dashboard</p>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    @endif
     <div id="calendar"></div>
 @endsection
