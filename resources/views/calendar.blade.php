0389d8357d4575b3504589fde4a8cea366484d65
marlie.dueck <marlie.dueck@sd27.bc.ca>
2020-09-21 10:19:18 -0700

    Added node module Autolinker to automatically add hyperlinks to links

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 
 @section('content')
+    <script src="{{ asset('/js/calendar.js') }}"></script>
     @if(Auth::user()->isAdmin())
         <div class="container">
             <a href="{{ route('events') }}">List all event deadlines..</a>
