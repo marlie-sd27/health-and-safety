bc0ddf450384ef7bb7a032f8ff1188ae96d771fb
marlie.dueck <marlie.dueck@sd27.bc.ca>
2020-11-26 14:28:25 -0800

    Calendar shows all events for admins and assigned events for everyone else

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,38 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Assignments;
 use App\Events;
 use App\Helpers\Helper;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EventsController extends Controller
 {
 
     public function ajax(Request $request)
     {
+        // get request input parameters
         $start = $request->filled('start') ? $request->query('start') : null;
         $end = $request->filled('end') ? $request->query('end') : null;
 
-        // query for the events between start and end date
-        $events = Events::with('forms')
-            ->when($start, function ($query, $start) {
-                return $query->where('date', '>', $start);
-            })
-            ->when($end, function ($query, $end) {
-                return $query->where('date', '<', $end);
-            })
-            ->get();
+        // if user is admin, return all deadlines
+        if (Auth::user()->isAdmin()) {
+
+            // query for the events between start and end date
+            $events = Events::with('forms')
+                ->when($start, function ($query, $start) {
+                    return $query->where('date', '>', $start);
+                })
+                ->when($end, function ($query, $end) {
+                    return $query->where('date', '<', $end);
+                })
+                ->get();
 
 
-        // add attribute url for links and title for displaying in calendar
-        foreach ($events as $event) {
-            $event['url'] = route('forms.show', ['form' => $event->forms_id, 'event' => $event->id]);
-            $event['title'] = $event->forms->title;
+            // add attribute url for links and title for displaying in calendar
+            foreach ($events as $event) {
+                $event['url'] = route('forms.show', ['form' => $event->forms_id, 'event' => $event->id]);
+                $event['title'] = $event->forms->title;
+            }
+
+            return response()->json($events);
+        } // otherwise, return only the user's assigned deadlines
+        else {
+            $events = Events::with('forms')
+                ->join('assignments', 'assignments.events_id', '=', 'events.id')
+                ->where('assignments.email', Auth::user()->email)
+                ->get();
+
+            // add attribute url for links and title for displaying in calendar
+            foreach ($events as $event) {
+                $event['url'] = route('forms.show', ['form' => $event->forms_id, 'event' => $event->id]);
+                $event['title'] = $event->forms->title;
+            }
+
+            return response()->json($events);
         }
-
-        $events = Helper::filterEvents($events);
-
-        return response()->json($events);
     }
 
 
