b8ba097f1167dc0ae2119b02aa7a3ec7f9101296
marlie.dueck <marlie.dueck@sd27.bc.ca>
2020-11-30 09:54:32 -0800

    search function for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,12 +5,10 @@ namespace App\Http\Controllers;
 
 use App\Helpers\CollectionHelper;
 use App\Helpers\GraphAPIHelper;
-use App\TokenStore\TokenCache;
 use App\User;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\Request;
-use Microsoft\Graph\Graph;
-use Microsoft\Graph\Model;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
@@ -23,20 +21,28 @@ class UserController extends Controller
 
     public function index(Request $request)
     {
-        $name = $request->filled('name') ? $request->name : null;
+        // get the optional search parameter
+        $name = $request->filled('name') ? strtolower($request->name) : null;
 
-
+        // check if users is stored in session. If not, send request to Azure and get all staff.
+        // Store in collection for easier use of methods (ex, filter, paginate, sort)
         $users = session('users') ?? collect(GraphAPIHelper::getAllStaff());
 
-        if ($name)
+        // set session variable users for loading users more efficiently with further requests
+        session(['users' => $users]);
+
+
+        // if name parameter is set, filter users by name
+        if($name)
         {
-            $users = $users->filter( function ($value, $key) {
-                dd($value->getMail());
-                return strpos($value->getMail(), 'marlie') !== false;
+            $users = $users->filter(function ($value) use ($name) {
+                // if mail address exists for user, check if it contains the name search parameter to filter
+                if($value->getMail())
+                {
+                    return str_contains(strtolower($value->getMail()), $name);
+                } else return false;
             });
         }
-
-        session(['users' => $users]);
 
         return view('Manage/users', ['users' => CollectionHelper::paginate($users, 25), 'name' => $name]);
     }
