37fd086719f0c8732f4578403a4082e0fbf6bd68
marlie.dueck <marlie.dueck@sd27.bc.ca>
2020-12-07 12:09:13 -0800

    add legend to calendar

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -2,10 +2,24 @@
 
 @section('content')
     <script src="{{ asset('/js/calendar.js') }}"></script>
-    @if(Auth::user()->isAdmin())
+    @if($admin)
         <div class="container">
             <a href="{{ route('events') }}">List all event deadlines..</a>
         </div>
+    @else
+        <nav class="navbar navbar-expand-md">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item padding-right">
+                    <p> <i class="fa fa-square upcoming"></i> Upcoming</p>
+                </li>
+                <li class="nav-item padding-right">
+                    <p> <i class="fa fa-square complete"></i> Complete</p>
+                </li>
+                <li class="nav-item padding-right">
+                    <p> <i class="fa fa-square overdue"></i> Overdue</p>
+                </li>
+            </ul>
+        </nav>
     @endif
     <div id="calendar"></div>
 @endsection
