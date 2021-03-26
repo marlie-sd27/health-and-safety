93d7cfb23b408ddb1900e4d35e7e3beb18a23cbe
marlie.dueck <marlie.dueck@sd27.bc.ca>
2021-03-26 11:09:51 -0700

    fix dashboard overdue section for principals

--- app/Helpers/QueryHelper.php
+++ app/Helpers/QueryHelper.php
@@ -81,7 +81,7 @@ class QueryHelper
                 return $query->where('assignments.sites_id', $site_due);
             })
             ->orderBy('date', 'asc')
-            ->select('events.*', 'assignments.email', 'forms.title', 'sites.site','sites.code', 'assignments.sites_id')
+            ->select('events.*', 'assignments.email', 'forms.title', 'sites.site','sites.code', 'assignments.sites_id','events.date')
             ->when($paginate, function ($query, $paginate) {
                 return $query->paginate($paginate, ['*'], 'overdue');
             }, function($query) {
